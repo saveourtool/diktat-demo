5c97ac6dd924633bf3927d86f0c43dbc25213753
petertrr <peter.trifanov@mail.ru>
2020-11-16 16:30:51 +0300

    Updated diktat to 0.1.4
    ### What was done:
    * Updated diktat
    * Use diktat-gradle-plugin
    * Minor fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("multiplatform") version "1.4.10"
     kotlin("plugin.spring") version "1.4.10"
     id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("org.cqfn.diktat.diktat-gradle-plugin") version "0.1.4"
 }
 
 repositories {
@@ -13,9 +14,9 @@ repositories {
 }
 
 val kotlinVersion = "1.4.10"
-val diktatVersion = "0.1.3"
+val diktatVersion = "0.1.4"
 val ktlintVersion = "0.39.0"
-val springBootVersion = "2.3.1.RELEASE"
+val springBootVersion = "2.4.0"
 
 publishing {
     publications {
@@ -99,26 +100,6 @@ tasks.getByName<BootJar>("bootJar") {
     requiresUnpack("**/kotlin-compiler-embeddable-*.jar")
 }
 
-val ktlint: Configuration by configurations.creating
-val outputDir = "${project.buildDir}/reports/diktat/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
-val diktatCheck by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    description = "Check Kotlin code style."
-    classpath = ktlint
-    main = "com.pinterest.ktlint.Main"
-
-    // specify proper path to sources that should be checked here
-    args = listOf("src/*/kotlin/**/*.kt")
-    dependencies {
-        ktlint("com.pinterest:ktlint:$ktlintVersion") {
-            // need to exclude standard ruleset to use only diktat rules
-            exclude("com.pinterest.ktlint", "ktlint-ruleset-standard")
-        }
-
-        // diktat ruleset
-        ktlint("org.cqfn.diktat:diktat-rules:$diktatVersion")
-    }
+diktat {
+    inputs = files("src/*/kotlin/**/*.kt")
 }
