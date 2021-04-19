5ca53afcb286901dcebc3b599a4e0679d4b94a2b
petertrr <peter.trifanov@mail.ru>
2021-04-19 15:22:14 +0300

    spring-kofu
    ### What's done:
    * Bump dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ kotlin {
         getByName("jvmMain") {
             dependencies {
                 implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-                implementation("org.springframework.fu:spring-fu-kofu:0.4.3")
+                implementation("org.springframework.fu:spring-fu-kofu:0.4.4")
                 implementation("org.cqfn.diktat:diktat-common:$diktatVersion") {
                     // exclude to use logback provided by spring
                     exclude("org.slf4j", "slf4j-log4j12")
